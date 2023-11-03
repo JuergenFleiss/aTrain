fb1a7785f55edce840f534a9af037dcc8bd55cf5
ArminHaberl <haberlarmin@gmx.at>
2023-11-03 13:44:10 +0100

    add srt output

--- aTrain/output_files.py
+++ aTrain/output_files.py
@@ -1,16 +1,19 @@
 import os
 import json 
 import pandas as pd
+import time
+
+def create_output_files(result, speaker_detection, file_directory, orig_filename):
+        create_json_file(file_directory,outfile_name="transcription.json",content=result)
+        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=False, timestamps = False)
+        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=False, timestamps = True)
+        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=True, timestamps = True)
+        create_srt_file(result, file_directory)
 
 def create_json_file(file_directory, outfile_name, content):
         output_file_text = os.path.join(file_directory,outfile_name)
         with open(output_file_text,"w", encoding="utf-8") as json_file:
             json.dump(content, json_file,ensure_ascii=False)
-
-def create_txt_files (result, speaker_detection, file_directory, orig_filename):
-        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=False, timestamps = False)
-        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=False, timestamps = True)
-        create_txt_file(result, file_directory, orig_filename, speaker_detection, maxqda=True, timestamps = True)
 
 def create_txt_file (result,file_directory, orig_filename, speaker_detection, timestamps, maxqda):
     segments = result["segments"]
@@ -30,18 +33,22 @@ def create_txt_file (result,file_directory, orig_filename, speaker_detection, ti
                 current_speaker = speaker
             text = str(segment["text"]).lstrip()
             if timestamps:
-                time = segment["start"]
-                formatted_time = format_time(time)
-                text = f"{formatted_time} - {text}"
+                start_time = time.strftime("[%H:%M:%S]", time.gmtime(segment["start"]))
+                text = f"{start_time} - {text}"
             file.write(text + (" " if maxqda else  "\n"))
 
-def format_time(time):
-    seconds = int(time)
-    hours = seconds // 3600  # Divide by 3600 to get the number of hours
-    minutes = (seconds % 3600) // 60  # Divide the remaining seconds by 60 to get the number of minutes
-    remaining_seconds = seconds % 60  # The remaining seconds
-    formatted_time = "[{:02d}:{:02d}:{:02d}]".format(hours, minutes, remaining_seconds)
-    return formatted_time
+def create_srt_file(result,file_directory):
+    segments = result["segments"]
+    file_path = os.path.join(file_directory, "transcription.srt")
+    with open(file_path,"w", encoding="utf-8") as srt_file:
+        for index, segment in enumerate(segments,1):
+            srt_file.write(f"{index}\n")
+            start_time = segment["start"]
+            end_time = segment["end"]
+            start_time_format = time.strftime("%H:%M:%S", time.gmtime(start_time)) + f",{round((start_time-int(start_time))*1000):03}"
+            end_time_format = time.strftime("%H:%M:%S", time.gmtime(end_time)) + f",{round((end_time-int(end_time))*1000):03}"
+            srt_file.write(f"{start_time_format} --> {end_time_format}\n")
+            srt_file.write(f"{str(segment['text']).lstrip()}\n\n")
 
 def transform_speakers_results(diarization_segments):    
     diarize_df = pd.DataFrame(diarization_segments.itertracks(yield_label=True))
