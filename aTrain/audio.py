a44a5f0c29e40d727537909ad34f2c11a7708be3
ArminHaberl <haberlarmin@gmx.at>
2023-11-03 17:25:50 +0100

    custom ffmpeg-python to stop bug during freezing

--- aTrain/audio.py
+++ aTrain/audio.py
@@ -1,4 +1,5 @@
 from .load_resources import get_ffmpeg
+from .custom_ffmpeg import run
 import os
 import ffmpeg
 from scipy.io import wavfile
@@ -10,7 +11,7 @@ def prepare_audio (file_id,file_path,file_directory):
     output_path =  os.path.join(file_directory,output_file)
     stream = ffmpeg.input(file_path)
     stream = ffmpeg.output(stream, output_path)
-    ffmpeg.run(stream,quiet=True, cmd=ffmpeg_path)
+    run(stream,quiet=True, cmd=ffmpeg_path)
     return output_path
 
 def get_audio_duration(file_path):
@@ -30,11 +31,9 @@ def format_duration(time):
 def load_audio(file: str, sr: int = 16000):
     ffmpeg_path = get_ffmpeg()
     try:
-        out, _ = (
-            ffmpeg.input(file, threads=0)
-            .output("-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
-            .run(cmd=[str(ffmpeg_path), "-nostdin"], capture_stdout=True, capture_stderr=True)
-        )
+        stream = ffmpeg.input(file, threads=0)
+        stream = ffmpeg.output(stream,"-", format="s16le", acodec="pcm_s16le", ac=1, ar=sr)
+        out, _ = run(stream,cmd=[str(ffmpeg_path), "-nostdin"], capture_stdout=True, capture_stderr=True) 
     except ffmpeg.Error as e:
         raise RuntimeError(f"Failed to load audio: {e.stderr.decode()}") from e
     return np.frombuffer(out, np.int16).flatten().astype(np.float32) / 32768.0
