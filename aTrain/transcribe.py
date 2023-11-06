938d4a57dfd42eb835f4afdc227b2f499e039743
ArminHaberl <haberlarmin@gmx.at>
2023-11-06 16:01:36 +0100

    remove whisperx dependency, update pyannote

--- aTrain/transcribe.py
+++ aTrain/transcribe.py
@@ -2,6 +2,7 @@ from .output_files import create_output_files, named_tuple_to_dict, transform_sp
 from .archive import read_metadata, delete_transcription, add_processing_time_to_metadata, TRANSCRIPT_DIR
 from .audio import load_audio
 from .load_resources import get_model
+import numpy as np
 import os
 import traceback
 from flask import render_template
@@ -31,7 +32,6 @@ def handle_transcription(file_id):
 def transcribe (audio_file, model, language, speaker_detection, num_speakers, device, compute_type):   
     import gc, torch #Import inside the function to speed up the startup time of the destkop app.
     from faster_whisper import WhisperModel
-    from whisperx import assign_word_speakers
     from .pipeline import CustomPipeline
     
     language = None if language == "auto-detect" else language
@@ -55,13 +55,34 @@ def transcribe (audio_file, model, language, speaker_detection, num_speakers, de
     if speaker_detection:
         yield {"task":"Loading speaker detection model"}
         model_path = get_model("diarize")
-        diarize_model = CustomPipeline.from_pretrained(model_path).to(device)
+        diarize_model = CustomPipeline.from_pretrained(model_path).to(torch.device(device))
         yield {"task":"Detecting speakers"}
         diarization_segments = diarize_model(audio_file,min_speakers=min_speakers, max_speakers=max_speakers)
         speaker_results = transform_speakers_results(diarization_segments)
         del diarize_model; gc.collect(); torch.cuda.empty_cache()
         transcript_with_speaker = assign_word_speakers(speaker_results,transcript)
         yield {"task":"Finishing up", "result":transcript_with_speaker}
+
+def assign_word_speakers(diarize_df, transcript_result, fill_nearest=False):
+    #Function from whisperx -> see https://github.com/m-bain/whisperX.git
+    transcript_segments = transcript_result["segments"]
+    for seg in transcript_segments:
+        diarize_df['intersection'] = np.minimum(diarize_df['end'], seg['end']) - np.maximum(diarize_df['start'], seg['start'])
+        diarize_df['union'] = np.maximum(diarize_df['end'], seg['end']) - np.minimum(diarize_df['start'], seg['start'])
+        dia_tmp = diarize_df[diarize_df['intersection'] > 0] if not fill_nearest else diarize_df
+        if len(dia_tmp) > 0:
+            speaker = dia_tmp.groupby("speaker")["intersection"].sum().sort_values(ascending=False).index[0]
+            seg["speaker"] = speaker
+        if 'words' in seg:
+            for word in seg['words']:
+                if 'start' in word:
+                    diarize_df['intersection'] = np.minimum(diarize_df['end'], word['end']) - np.maximum(diarize_df['start'], word['start'])
+                    diarize_df['union'] = np.maximum(diarize_df['end'], word['end']) - np.minimum(diarize_df['start'], word['start'])
+                    dia_tmp = diarize_df[diarize_df['intersection'] > 0] if not fill_nearest else diarize_df
+                    if len(dia_tmp) > 0:
+                        speaker = dia_tmp.groupby("speaker")["intersection"].sum().sort_values(ascending=False).index[0]
+                        word["speaker"] = speaker
+    return transcript_result    
 
 if __name__ == "__main__":
     ...
