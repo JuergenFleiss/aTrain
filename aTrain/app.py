4b0b8d6f2ad7e3b68a807e0d70d32e2f7a803946
ArminHaberl <haberlarmin@gmx.at>
2023-11-06 13:47:04 +0100

    remove debug statement

--- aTrain/app.py
+++ aTrain/app.py
@@ -96,7 +96,6 @@ def run_app():
     app_height = int(min([monitor.height for monitor in get_monitors()])*0.8)
     app_width = int(min([monitor.width for monitor in get_monitors()])*0.8)
     try:
-        int("asdf")
         webview.create_window("aTrain",app,height=app_height,width=app_width)
         with keep.running():
             webview.start()
