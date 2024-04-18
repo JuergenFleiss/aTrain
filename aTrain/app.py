6010b3ebdeeb4b905cc42760904815363b049a13
ArminHaberl <haberlarmin@gmx.at>
2024-04-18 20:08:19 +0200

    remove unused code

--- aTrain/app.py
+++ aTrain/app.py
@@ -1,18 +1,17 @@
-from .archive import read_archive, create_metadata, delete_transcription, open_file_directory, TIMESTAMP_FORMAT, ATRAIN_DIR
+from .archive import read_archive, delete_transcription, open_file_directory
 from .version import __version__
-from flask import Flask, render_template, request, redirect, stream_with_context
+from .settings import load_settings
+from flask import Flask, render_template, request, redirect
 from importlib.resources import files
 from screeninfo import get_monitors
-import os
 import webview
 from wakepy import keep
 import traceback
 import yaml
 import time
-from datetime import datetime
-import webbrowser
 import argparse
 
+#-----Setup------#
 app = Flask(__name__)
 
 @app.template_filter
@@ -21,7 +20,9 @@ def format_duration(duration):
 
 @app.context_processor
 def set_globals():
-    return dict(version=__version__)
+    return dict(version=__version__, settings=load_settings())
+
+#-----Routes------#
 
 @app.get("/")
 def render_home():
@@ -39,42 +40,19 @@ def render_faq():
         faqs = yaml.safe_load(faq_file)
     return render_template("pages/faq.html", faqs = faqs)
 
-@app.get("/settings")
-def get_default_settings():
-    settings_file_path = os.path.join(ATRAIN_DIR,"settings.txt")
-    if os.path.exists(settings_file_path):
-        with open(settings_file_path,"r", encoding='utf-8') as settings_file:
-            settings = yaml.safe_load(settings_file)
-            cuda_available = settings["cuda"]
-    else:
-        from torch import cuda
-        cuda_available = cuda.is_available()
-        settings = {"cuda":cuda_available}
-        os.makedirs(ATRAIN_DIR, exist_ok=True)
-        with open(settings_file_path,"w", encoding='utf-8') as settings_file:
-            yaml.safe_dump(settings,settings_file)
-    return render_template("modals/modal_settings.html", cuda_available=cuda_available)
-
-@app.post("/upload")
+@app.post("/transcribe")
 def upload():
-    timestamp = datetime.now().strftime(TIMESTAMP_FORMAT)
-    try:
-        file, model, language, speaker_detection, num_speakers, device, compute_type = get_inputs(request) 
-        inputs_correct = check_inputs(file, model, language, num_speakers)
-        if inputs_correct is False:
-            return render_template("modals/modal_wrongInput.html")
-        filename, file_id, estimated_process_time, audio_duration = handle_file(file, timestamp, model, device)
-        create_metadata(file_id, filename, audio_duration, model, language, speaker_detection, num_speakers, device, compute_type, timestamp)
-        return render_template("modals/modal_process.html", id=file_id, task="Processing file", total_duration=estimated_process_time, device=device)
-    except Exception as error:
-        delete_transcription(timestamp)
-        traceback_str = traceback.format_exc()
-        error = str(error)
-        return render_template("modals/modal_error.html",error=error, traceback=traceback_str)
+    
+    return render_template("modals/modal_wrongInput.html")
 
-@app.route("/transcription/<upload_id>")
-def stream_data(upload_id):
-    return stream_with_context(handle_transcription(upload_id)), {"Content-Type": "text/event-stream"}
+    #return render_template("modals/modal_process.html", id=file_id, task="Processing file", total_duration=estimated_process_time, device=device)
+
+    #except Exception as error:
+    #    delete_transcription(timestamp)
+    #    traceback_str = traceback.format_exc()
+    #    error = str(error)
+    #    return render_template("modals/modal_error.html",error=error, traceback=traceback_str)
+
 
 @app.route('/open/<file_id>')
 def open_transcription(file_id):
@@ -91,14 +69,6 @@ def delete_archive_files(file_id):
 def revert_latest_changes(upload_id):
     delete_transcription(upload_id)
     return redirect(request.referrer)
-
-@app.get("/openbrowser/<website>")
-def open_browser(website):
-    match website:
-        case "github": url = "https://github.com/BANDAS-Center/aTrain"
-        case "feedback": url = "https://survey.uni-graz.at/index.php/381219"
-    webbrowser.open_new(url)
-    return ""
 
 def run_app():
     app_height = int(min([monitor.height for monitor in get_monitors()])*0.8)
