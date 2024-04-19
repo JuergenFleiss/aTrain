08d0830af806a7a29091c1935060786b815fa8ae
ArminHaberl <haberlarmin@gmx.at>
2024-04-19 13:13:55 +0200

    add temporary mockup file

--- aTrain/mockup_core.py
+++ aTrain/mockup_core.py
@@ -1,3 +1,8 @@
 #This is a mockup pipeline that will later be replaced by aTrain_core
+import time
+from .SSE import send_event
 
-
+def transcribe():
+    for i in range(500):
+        time.sleep(1)
+        send_event(i, event="test")
