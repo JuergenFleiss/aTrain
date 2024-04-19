b854c2b69c5b54316f1c75025baa80580f281ed0
ArminHaberl <haberlarmin@gmx.at>
2024-04-19 16:13:08 +0200

    add functions to the mockup core library

--- aTrain/mockup_core.py
+++ aTrain/mockup_core.py
@@ -1,8 +1,13 @@
 #This is a mockup pipeline that will later be replaced by aTrain_core
 import time
 from .SSE import send_event
+from .globals import SERVER_EVENTS
 
 def transcribe():
     for i in range(500):
         time.sleep(1)
-        send_event(i, event="test")
+        print(i)
+        send_event(i, event=SERVER_EVENTS.task)
+
+def check_inputs():
+    return True
