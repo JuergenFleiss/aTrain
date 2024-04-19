44ca45893addca64783faa05d7c58808d01e5bce
ArminHaberl <haberlarmin@gmx.at>
2024-04-19 12:26:29 +0200

    update SSE logic

--- aTrain/SSE.py
+++ aTrain/SSE.py
@@ -1,26 +1,42 @@
-from queue import Queue
+from queue import Queue, Full, Empty
 from blinker import signal
 
-EVENT_SIGNAL = signal("SSE")
-EVENT_QUEUE = Queue(maxsize=5)
-STOP_QUEUE = Queue(maxsize=1)
+def send_event(data, event : str = "message", channel = "global"):
+    event_string = f"event: {event}\ndata: {data}\n\n"
+    event_signal = signal(channel)
+    event_signal.send(event_string)
 
-@EVENT_SIGNAL.connect
-def add_to_EVENT_QUEUE(data):
-    EVENT_QUEUE.put(data)
+def stream_events(channel = "global", queue_size = 0):
+    event_queue = Queue(maxsize=queue_size)
+    event_signal = signal(channel)
+    
+    stop_queue = Queue(maxsize=1)
+    stop_signal = signal(f"stop_{channel}")
 
-def send_event(data, event : str = None):
-    event = event if event else "data"
-    event_string = f"{event}: {data}\n\n"
-    EVENT_SIGNAL.send(event_string)
-
-def stream_events():
-    while STOP_QUEUE.empty():
+    def add_to_event_queue(data):
         try:
-            event = EVENT_QUEUE.get_nowait()
+            event_queue.put_nowait(data)
+        except Full:
+            stop_signal.send("stop")
+            event_signal.disconnect(add_to_event_queue)
+
+    event_signal.connect(add_to_event_queue)
+
+    def add_to_stop_queue(data):
+        try:
+            stop_queue.put_nowait(data)
+        except Full:
+            stop_signal.disconnect(add_to_stop_queue)
+
+    stop_signal.connect(add_to_stop_queue)
+
+    while stop_queue.empty():
+        try:
+            event = event_queue.get_nowait()
             yield event
-        except:
+        except Empty:
             continue
 
-def stop_SSE():
-    STOP_QUEUE.put("stop")
+def stop_SSE(channel = "global"):
+    stop_signal = signal(f"stop_{channel}")
+    stop_signal.send("stop")
