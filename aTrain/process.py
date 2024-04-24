d821014c3d4f372ecc1ec58257ae7ad3f253e529
ArminHaberl <haberlarmin@gmx.at>
2024-04-24 15:54:37 +0200

    migrate to EventSender class

--- aTrain/process.py
+++ aTrain/process.py
@@ -1,41 +1,44 @@
-from multiprocessing import queues, get_context
-from queue import Empty
-from typing import Literal
+from multiprocessing import Queue
+from queue import Empty, Full
+from typing import Literal, List
 
 SERVER_EVENTS = Literal["error", "progress_value" , "progress_max", "task" , "finished" , "wrong_input"]
-
-class EventQueue(queues.Queue):
-    def __init__(self, *args, **kwargs):
-        ctx = get_context()  # Get the default context
-        super().__init__(ctx=ctx, *args, **kwargs)  # Pass the context to Queue
-
-    def send(self, data, event : SERVER_EVENTS):
-        event_string = f"event: {event}\ndata: {data}\n\n"
-        try:
-            self.put_nowait(event_string)
-        except Exception as error:
-            print("Encountered an error:", error)
-
-event_queue = EventQueue()
-stop_queue = EventQueue(maxsize=1)
-running_processes = []
-
-def stream_events():
-    while stop_queue.empty():
-        try:
-            event = event_queue.get_nowait()
-            yield event
-        except Empty:
-            continue
-
-def stop_stream():
-    stop_queue.put("stop")
+RUNNING_PROCESSES = []
 
 def kill_all_processes():
     """Terminate all running processes."""
-    for process in running_processes:
+    for process in RUNNING_PROCESSES:
         process.terminate()
         print(f"Terminated {process.name}.")
-    running_processes.clear()
+    RUNNING_PROCESSES.clear()
     print("All processes have been terminated.")
+
+class EventSender:
+    def __init__(self, maxsize = 10):
+        self.listeners : List[Queue]= []
+        self.maxsize = maxsize
+        self.stopper : Queue = Queue(maxsize=1)
+
+    def stream(self):
+        listener = Queue(maxsize=self.maxsize)
+        self.listeners.append(listener)
+        while self.stopper.empty():
+            try:
+                event = listener.get_nowait()
+                yield event
+            except Empty:
+                continue
+
+    def send(self, data, event : SERVER_EVENTS):
+        event_string = f"event: {event}\ndata: {data}\n\n"
+        for i in reversed(range(len(self.listeners))):
+            try:
+                self.listeners[i].put_nowait(event_string)
+            except Full:
+                del self.listeners[i]
+
+    def stop(self):
+        self.stopper.put("stop")
+
+event_sender = EventSender()
 
