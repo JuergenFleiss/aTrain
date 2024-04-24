fb8655dcdd2fb3d34fc2071ff2948e356d1600a7
ArminHaberl <haberlarmin@gmx.at>
2024-04-24 14:16:25 +0200

    remove server events from globals

--- aTrain/globals.py
+++ aTrain/globals.py
@@ -1,5 +1,4 @@
 import os
-from dataclasses import dataclass
 
 USER_DIR = os.path.expanduser("~")
 DOCUMENTS_DIR = os.path.join(USER_DIR,"Documents")
@@ -8,14 +7,3 @@ TRANSCRIPT_DIR = os.path.join(ATRAIN_DIR,"transcriptions")
 SETTINGS_FILE = os.path.join(ATRAIN_DIR,"settings.txt")
 METADATA_FILENAME = "metadata.txt"
 TIMESTAMP_FORMAT = "%Y-%m-%d %H-%M-%S"
-
-@dataclass
-class ServerEvents:
-    error: str = "transcription_error"
-    wrong_input : str = "wrong_input"
-    progress_total : str = "total_progress"
-    progress : str = "update_progress"
-    task : str = "update_task"
-    finished : str = "transcription_finished"
-
-SERVER_EVENTS = ServerEvents()
