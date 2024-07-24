81c9d11257bad0349a5a6d6fbf4bc1630e2783f9
ArminHaberl <haberlarmin@gmx.at>
2024-07-24 14:52:28 +0200

    refactor archive loading

--- aTrain/archive.py
+++ aTrain/archive.py
@@ -6,44 +6,63 @@ from importlib.resources import files
 from aTrain_core.globals import TRANSCRIPT_DIR, METADATA_FILENAME
 
 
-def read_archive():
-    os.makedirs(TRANSCRIPT_DIR, exist_ok=True)
-    all_file_directories = [directory.name for directory in os.scandir(
-        TRANSCRIPT_DIR) if directory.is_dir()]
-    all_file_directories.sort(reverse=True)
-    all_metadata = []
-    for directory_name in all_file_directories:
-        metadata_file_path = os.path.join(
-            TRANSCRIPT_DIR, directory_name, METADATA_FILENAME)
-        if os.path.exists(metadata_file_path):
-            with open(metadata_file_path, "r", encoding="utf-8") as metadata_file:
-                metadata = yaml.safe_load(metadata_file)
-                metadata["file_id"] = directory_name
-            all_metadata.append(metadata)
-        else:
-            metadata = {
-                "file_id": directory_name,
-                "filename": directory_name[20:] if len(directory_name) > 20 else "-",
-                "timestamp": directory_name[:20] if len(directory_name) >= 20 else "-"
-            }
-            all_metadata.append(metadata)
+def read_archive() -> list:
+    all_directories = read_directories()
+    all_metadata = read_all_metadata(all_directories)
     return all_metadata
 
 
-def delete_transcription(file_id):
+def read_directories() -> list:
+    os.makedirs(TRANSCRIPT_DIR, exist_ok=True)
+    directories = [directory.name for directory in os.scandir(
+        TRANSCRIPT_DIR) if directory.is_dir()]
+    directories.sort(reverse=True)
+    return directories
+
+
+def read_all_metadata(all_directories):
+    all_metadata = []
+    for directory in all_directories:
+        metadata_file_path = os.path.join(
+            TRANSCRIPT_DIR, directory, METADATA_FILENAME)
+        if os.path.exists(metadata_file_path):
+            metadata = read_metadata_file(metadata_file_path, directory)
+        else:
+            metadata = metadata_from_dir_name(directory)
+        all_metadata.append(metadata)
+    return all_metadata
+
+
+def read_metadata_file(metadata_file_path, directory):
+    with open(metadata_file_path, "r", encoding="utf-8") as metadata_file:
+        metadata = yaml.safe_load(metadata_file)
+        metadata["file_id"] = directory
+    return metadata
+
+
+def metadata_from_dir_name(directory):
+    metadata = {
+        "file_id": directory,
+        "filename": directory[20:] if len(directory) > 20 else "-",
+        "timestamp": directory[:20] if len(directory) >= 20 else "-"
+    }
+    return metadata
+
+
+def delete_transcription(file_id) -> None:
     file_id = "" if file_id == "all" else file_id
-    directory_name = os.path.join(TRANSCRIPT_DIR, file_id)
-    if os.path.exists(directory_name):
-        shutil.rmtree(directory_name)
+    directory = os.path.join(TRANSCRIPT_DIR, file_id)
+    if os.path.exists(directory):
+        shutil.rmtree(directory)
     if not os.path.exists(TRANSCRIPT_DIR):
         os.makedirs(TRANSCRIPT_DIR, exist_ok=True)
 
 
-def open_file_directory(file_id):
+def open_file_directory(file_id) -> None:
     file_id = "" if file_id == "all" else file_id
-    directory_name = os.path.join(TRANSCRIPT_DIR, file_id)
-    if os.path.exists(directory_name):
-        show_in_file_manager(directory_name)
+    directory = os.path.join(TRANSCRIPT_DIR, file_id)
+    if os.path.exists(directory):
+        show_in_file_manager(directory)
 
 
 def load_faqs():
