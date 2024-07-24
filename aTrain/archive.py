77b63c5734604f74efddcadbc803d04d3c8f832a
ArminHaberl <haberlarmin@gmx.at>
2024-07-24 14:56:01 +0200

    add docstrings

--- aTrain/archive.py
+++ aTrain/archive.py
@@ -7,12 +7,14 @@ from aTrain_core.globals import TRANSCRIPT_DIR, METADATA_FILENAME
 
 
 def read_archive() -> list:
+    """A function that reads all past transcriptions still located in the archive."""
     all_directories = read_directories()
     all_metadata = read_all_metadata(all_directories)
     return all_metadata
 
 
 def read_directories() -> list:
+    """A function that returns a list of all directories in the archive folder"""
     os.makedirs(TRANSCRIPT_DIR, exist_ok=True)
     directories = [directory.name for directory in os.scandir(
         TRANSCRIPT_DIR) if directory.is_dir()]
@@ -21,6 +23,7 @@ def read_directories() -> list:
 
 
 def read_all_metadata(all_directories):
+    """A function that returns all available metadata from past transcriptions"""
     all_metadata = []
     for directory in all_directories:
         metadata_file_path = os.path.join(
@@ -34,6 +37,7 @@ def read_all_metadata(all_directories):
 
 
 def read_metadata_file(metadata_file_path, directory):
+    """A function that reads the content of a metadata file for a given transcription."""
     with open(metadata_file_path, "r", encoding="utf-8") as metadata_file:
         metadata = yaml.safe_load(metadata_file)
         metadata["file_id"] = directory
@@ -41,6 +45,7 @@ def read_metadata_file(metadata_file_path, directory):
 
 
 def metadata_from_dir_name(directory):
+    """A function that extracts metadata from the directory name in the archive."""
     metadata = {
         "file_id": directory,
         "filename": directory[20:] if len(directory) > 20 else "-",
@@ -50,6 +55,7 @@ def metadata_from_dir_name(directory):
 
 
 def delete_transcription(file_id) -> None:
+    """A function that deletes a past transcription form the archive."""
     file_id = "" if file_id == "all" else file_id
     directory = os.path.join(TRANSCRIPT_DIR, file_id)
     if os.path.exists(directory):
@@ -59,6 +65,7 @@ def delete_transcription(file_id) -> None:
 
 
 def open_file_directory(file_id) -> None:
+    """A function that opens the output from a past transcription in the file explorer."""
     file_id = "" if file_id == "all" else file_id
     directory = os.path.join(TRANSCRIPT_DIR, file_id)
     if os.path.exists(directory):
@@ -66,6 +73,7 @@ def open_file_directory(file_id) -> None:
 
 
 def load_faqs():
+    """A function that reads the content of the faq file."""
     faq_path = str(files("aTrain.static").joinpath("faq.yaml"))
     with open(faq_path, "r", encoding='utf-8') as faq_file:
         faqs = yaml.safe_load(faq_file)
