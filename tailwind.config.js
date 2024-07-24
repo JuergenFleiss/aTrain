94946c6b6518e2d24ccd6421c064a25eedb2d7aa
ArminHaberl <haberlarmin@gmx.at>
2024-07-24 11:44:57 +0200

    add tailwind config and standalone CLI tool

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./aTrain/templates/*.html", "./aTrain/templates/*/*.html", "./aTrain/static/JS/*.js"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
+
