b5de5fd1cc8e49803726ed158196d97b7786d781
andreaforster <97340144+chicadelica@users.noreply.github.com>
2024-08-30 16:22:46 +0200

    redirect to faqs if no permission to access document folder. todo:add answer and instruction to faqs

--- aTrain/routes.py
+++ aTrain/routes.py
@@ -3,8 +3,9 @@ from flask import Blueprint, render_template
 from .archive import load_faqs, read_archive
 from .globals import REQUIRED_MODELS
 from .models import model_languages, read_downloaded_models, read_model_metadata
-from .settings import load_settings
+from .settings import load_settings, check_access, show_permission_instructions
 from .version import __version__
+from aTrain_core.globals import DOCUMENTS_DIR
 
 routes = Blueprint("routes", __name__)
 
@@ -30,36 +31,40 @@ def set_globals():
 
 @routes.get("/")
 def home():
-    models = read_downloaded_models()  # Get the list of downloaded models
+    if check_access(DOCUMENTS_DIR):
+        models = read_downloaded_models()  # Get the list of downloaded models
 
-    try:
-        if "large-v3" in models:
-            default_model = "large-v3"
-        elif models:
-            default_model = models[
-                0
-            ]  # Fall back to the first model if any models are available
+        try:
+            if "large-v3" in models:
+                default_model = "large-v3"
+            elif models:
+                default_model = models[
+                    0
+                ]  # Fall back to the first model if any models are available
 
-        languages = model_languages(default_model)
-        print("There appear to be no models downloaded")
-        return render_template(
-            "routes/transcribe.html",
-            settings=load_settings(),
-            models=models,
-            languages=languages,
-            default_model=default_model,  # Pass the default model to the template
-        )
+            languages = model_languages(default_model)
+            print("There appear to be no models downloaded")
+            return render_template(
+                "routes/transcribe.html",
+                settings=load_settings(),
+                models=models,
+                languages=languages,
+                default_model=default_model,  # Pass the default model to the template
+            )
 
-    except KeyError:
-        default_model = None  # No models available
-        languages = {}
-        return render_template(
-            "routes/transcribe.html",
-            settings=load_settings(),
-            models=models,
-            languages=languages,
-            default_model=default_model,  # Pass the default model to the template
-        )
+        except KeyError:
+            default_model = None  # No models available
+            languages = {}
+            return render_template(
+                "routes/transcribe.html",
+                settings=load_settings(),
+                models=models,
+                languages=languages,
+                default_model=default_model,  # Pass the default model to the template
+            )
+    else:
+        show_permission_instructions()
+        return render_template("routes/faq.html", faqs=load_faqs())
 
 
 @routes.get("/archive")
