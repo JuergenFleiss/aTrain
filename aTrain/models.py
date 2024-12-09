74cf6a5f333ed634f4c9d072c26e6f738a5177f6
ArminHaberl <haberlarmin@gmx.at>
2024-12-09 16:55:40 +0100

    join thread to fix error

--- aTrain/models.py
+++ aTrain/models.py
@@ -227,6 +227,7 @@ def stop_all_downloads() -> None:
     download: StoppableThread
     for download, model in RUNNING_DOWNLOADS:
         download.stop()
+        download.join()
         remove_model(model)
     RUNNING_DOWNLOADS.clear()
     EVENT_SENDER.finished_info()
